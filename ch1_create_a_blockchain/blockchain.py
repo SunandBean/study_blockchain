31fd5936ed6077a75738b21e90557a3bcc1d50f3
sunandbean <sunbin.vincent.kim@gmail.com>
2022-12-23 18:51:17 +0900

    start to make class for our blockchain

--- ch1_create_a_blockchain/blockchain.py
+++ ch1_create_a_blockchain/blockchain.py
@@ -7,7 +7,10 @@ import json
 from flask import Flask, jsonify
 
 # Part 1 - Building a Blockchain
+class Blockchain:
+    def __init__(self):
+        self.chain = []
+        self.create_block(proof = 1, previous_hash = '0') # for genesis block
 
 
-
-# Part 2 - Mining our Blockhcain
+# Part 2 - Mining our Blockchain
