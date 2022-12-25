40e4e5c41cd10c2df2db0daa09f1092bfd55f744
sunandbean <sunbin.vincent.kim@gmail.com>
2022-12-25 11:31:05 +0900

    add add_transaction function in Blockchain class

--- ch2_create_a_crypto_currency/suncoin.py
+++ ch2_create_a_crypto_currency/suncoin.py
@@ -66,6 +66,15 @@ class Blockchain:
             block_index += 1
         
         return True
+    
+    def add_transaction(self, sender, receiver, amount):
+        self.transactions.append({
+            'sender' : sender,
+            'receiver' : receiver,
+            'amount' : amount,
+        })
+        previous_block = self.get_previous_block()
+        return previous_block['index'] + 1
 
 # Part 2 - Mining our Blockchain
 
