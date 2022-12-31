12c315eef9b48c65d3b09ca71ed25b0e822fa829
sunandbean <sunbin.vincent.kim@gmail.com>
2022-12-31 23:07:57 +0900

    initial script of solidity

---
+++ ch3_create_smart_contract/suncoin_ico.sol
@@ -0,0 +1,26 @@
+// SPDX-License-Identifier: UNLICENSED
+// Suncoins ICO
+
+// Version of compiler
+pragma solidity ^0.8.17;
+
+contract suncoin_ico {
+
+    // Introducing the maximum number of Suncoins available for sale
+    uint public max_suncoins = 1000000;
+
+    // Introducing the USD to Suncoins conversion rate
+    uint public usd_to_suncoins = 1000;
+
+    // Introducing the total numbr of Suncoins that have been bought by the investors
+    uint public total_suncoins_bought = 0;
+
+    // Mapping from the investor address to its equity in Suncoins and USD
+    mapping(address => uint) equity_suncoins;
+    mapping(address => uint) equity_usd;
+
+    // Checking if an investor can buy Suncoins
+    modifier can_buy_suncoins(uint usd_invested) {
+        require (usd_invested * usd_to_suncoins + total_suncoins_bought <= max_suncoins);
+    }
+}
